cc4a853e8b14f4476c780cfbbd438fdc5a3665c5
Nessa <la-nessa@mail.ru>
2022-12-05 21:46:16 +0300

    Ex001

---
+++ Ex001/Program.cs
@@ -0,0 +1,13 @@
+﻿int N = new Random().Next(10,99);
+Console.WriteLine(N);
+int firstDigit = N / 10;
+int secondDigit = N % 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(secondDigit);
+}
