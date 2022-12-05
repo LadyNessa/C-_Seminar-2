a25c8fcdc04b18e3ab7a5cc70fffdc9d58334dfa
Nessa <la-nessa@mail.ru>
2022-12-05 22:59:49 +0300

    Ex002

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -2,14 +2,28 @@
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
-int x = number2 % number1;
 
-if (x == 0)
+if (number1 > number2)
 {
-    Console.WriteLine("Второе число кратно первому");
+    if (number1 % number2 == 0)
+    {
+        Console.WriteLine("Первое число кратно второму");
+    }
+    else
+    {
+        Console.Write("Остаток: ");
+        Console.WriteLine(number1 % number2);
+    }
 }
 else
 {
-    Console.Write("Остаток: ");
-    Console.WriteLine(x);
+    if (number2 % number1 == 0)
+    {
+        Console.WriteLine("Второе число кратно первому");
+    }
+    else
+    {
+        Console.Write("Остаток: ");
+        Console.WriteLine(number2 % number1);
+    }
 }
