d92f352453902e3b03564f497b236fb27169bf21
Nessa <la-nessa@mail.ru>
2022-12-05 22:00:12 +0300

    Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int x = number2 % number1;
+
+if (x == 0)
+{
+    Console.WriteLine("Второе число кратно первому");
+}
+else
+{
+    Console.Write("Остаток: ");
+    Console.WriteLine(x);
+}
